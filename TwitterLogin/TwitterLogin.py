6ceaf5f2135355c9d4c55769885ecfeefdee985c
WaltXin <xiongx@tcd.ie>
2017-09-20 19:15:12 +0100

    Add Daft.ie web crawler

---
+++ TwitterLogin/TwitterLogin.py
@@ -0,0 +1,31 @@
+import mechanicalsoup
+
+URL = "https://twitter.com/login"
+LOGIN = "xiongxinwei6@gmail.com"
+PASSWORD = "xxw33242"
+TWITTER_NAME = "xinweixiong" # without @
+
+# Create a browser object
+browser = mechanicalsoup.Browser()
+
+# request Twitter login page
+login_page = browser.get(URL)
+
+# we grab the login form
+login_form = login_page.soup.find("form", {"class":"signin"})
+
+# find login and password inputs
+login_form.find("input", {"name": "session[username_or_email]"})["value"] = LOGIN
+login_form.find("input", {"name": "session[password]"})["value"] = PASSWORD
+
+# submit form
+response = browser.submit(login_form, login_page.url)
+
+# verify we are now logged in ( get username in webpage )
+user = response.soup.find("b", { "class" : "u-linkComplex-target" }).string
+print(user)
+
+if TWITTER_NAME in user:
+    print("You're connected as " + TWITTER_NAME)
+else:
+    print("Not connected")
