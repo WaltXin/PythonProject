3eec8795d5766485e871654b9ed2913aaa4db7b9
WaltXin <xiongx@tcd.ie>
2018-04-03 20:43:05 +0100

    Add Dijkstra_shortest_path_Heap

---
+++ Dijkstra_shortest_path_Heap/Dijkstra_shortest_path_Heap.py
@@ -0,0 +1,95 @@
+from collections import defaultdict
+
+def push(heap, item):
+    """Push item onto heap, maintaining the heap invariant."""
+    heap.append(item)
+    shiftdown(heap, 0, len(heap)-1)
+
+def pop(heap):
+    """Pop the smallest item off the heap, maintaining the heap invariant."""
+    lastelt = heap.pop()    # raises appropriate IndexError if heap is empty
+    if heap:
+        returnitem = heap[0]
+        heap[0] = lastelt
+        shiftup(heap, 0)
+        return returnitem
+    return lastelt
+
+# 'heap' is a heap at all indices >= startpos, except possibly for pos.  pos
+# is the index of a leaf with a possibly out-of-order value.  Restore the
+# heap invariant.
+def shiftdown(heap, startpos, pos):
+    newitem = heap[pos]
+    # Follow the path to the root, moving parents down until finding a place
+    # newitem fits.
+    while pos > startpos:
+        parentpos = (pos - 1) >> 1
+        parent = heap[parentpos]
+        if newitem < parent:
+            heap[pos] = parent
+            pos = parentpos
+            continue
+        break
+    heap[pos] = newitem
+
+def shiftup(heap, pos):
+    endpos = len(heap)
+    startpos = pos
+    newitem = heap[pos]
+    # Bubble up the smaller child until hitting a leaf.
+    childpos = 2*pos + 1    # leftmost child position
+    while childpos < endpos:
+        # Set childpos to index of smaller child.
+        rightpos = childpos + 1
+        if rightpos < endpos and not heap[childpos] < heap[rightpos]:
+            childpos = rightpos
+        # Move the smaller child up.
+        heap[pos] = heap[childpos]
+        pos = childpos
+        childpos = 2*pos + 1
+    # The leaf at pos is empty now.  Put newitem there, and bubble it up
+    # to its final resting place (by sifting its parents down).
+    heap[pos] = newitem
+    shiftdown(heap, startpos, pos)
+
+
+def dijkstra(edges, f, t):
+    g = defaultdict(list)
+    for l,r,c in edges:
+        g[l].append((c,r))
+
+    q, seen = [(0,f,())], set()
+    while q:
+        (cost,v1,path) = pop(q)
+        if v1 not in seen:
+            seen.add(v1)
+            path = (v1, path)
+            if v1 == t: return (cost, path)
+
+            for c, v2 in g.get(v1, ()):
+                if v2 not in seen:
+                    push(q, (cost+c, v2, path))
+
+    return float("inf")
+
+if __name__ == "__main__":
+    edges = [
+        ("A", "B", 7),
+        ("A", "D", 5),
+        ("B", "C", 8),
+        ("B", "D", 9),
+        ("B", "E", 7),
+        ("C", "E", 5),
+        ("D", "E", 15),
+        ("D", "F", 6),
+        ("E", "F", 8),
+        ("E", "G", 9),
+        ("F", "G", 11)
+    ]
+
+    print ("=== Dijkstra ===")
+    print (edges)
+    print ("A -> E:")
+    print (dijkstra(edges, "A", "E"))
+    print ("F -> G:")
+    print (dijkstra(edges, "F", "G"))
